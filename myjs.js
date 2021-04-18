b70507d1bb0829587c5bfbd871be6bf2b0d26619
Nowan000 <82499516+Nowan000@users.noreply.github.com>
2021-04-18 17:55:49 +0200

    Update myjs.js
    Ajout des commentaires (oops)

--- myjs.js
+++ myjs.js
@@ -1,5 +1,5 @@
 let send = document.querySelector("#envoyer");
-
+//Réponse au formulaire
 send.addEventListener("click", e =>{
     let sLastName = document.querySelector("#nom");
     let sFirstName = document.querySelector("#prenom");
@@ -19,6 +19,7 @@ send.addEventListener("click", e =>{
     let alcoolLove = sAlcLove.value;
     let nbEnfants = sNbEnfants.value
 
+    // Envoi selon réponse au menu déroulant
     if (nbEnfants === "0") {
         kids = "aucun enfant"
     } else if(nbEnfants === "1"){
@@ -29,6 +30,7 @@ send.addEventListener("click", e =>{
         kids = "plus de deux enfants"
     }
 
+    // Gestion de champs non renseignés du formulaire
     if(age == ""){
         age = "Non renseigné"
     }
@@ -39,6 +41,7 @@ send.addEventListener("click", e =>{
         comments = "Non renseigné"
     }
 
+    //Bouton radios
     if(vikingLove === "oui"){
         love = "Aime les vikings"
     } else {
@@ -51,6 +54,7 @@ send.addEventListener("click", e =>{
         alcLove = "N'aime pas l'alcool"
     }
 
+    //alert avec les champs du formulaire
     if(lastName != "" && firstName != ""){
         alert("Information renseignées :" + "\n"
         + "Nom: " + lastName + "\n" 
